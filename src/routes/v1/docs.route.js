01343bf31d5cfaf51e3c51069721ce7abba98840
rrozek <jakubrojek@gmail.com>
2023-08-01 10:22:11 +0200

    [docs] add OpenAPI documentation along with swagger endpoint for easy testing

--- src/routes/v1/docs.route.js
+++ src/routes/v1/docs.route.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 const specs = swaggerJsdoc({
   swaggerDefinition,
-  apis: ['src/docs/*.yml', 'src/routes/v1/*.js'],
+  apis: ['docs/openapi/*.yaml'],
 });
 
 router.use('/', swaggerUi.serve);
